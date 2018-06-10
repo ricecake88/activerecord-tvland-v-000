a52e8588a2fa0f06052c93358dd11b8aba734349
Grace Shih <vocalise@gmail.com>
2018-06-10 05:45:20 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  def build_network
+  def build_network(attributes)
+    Network.new(attributes)
   end
 end
