5eba92391b03f0c70f8214621bd2ef26892374e4
Grace Shih <vocalise@gmail.com>
2018-06-10 05:43:38 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,7 +6,7 @@ class Character < ActiveRecord::Base
     "#{self.name}" + " always says: " + "#{self.catchphrase}"
   end
 
-  def build_network(attributes)
+  def build_show(attributes)
     binding.pry
     Network.new(attributes)
   end
